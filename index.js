23e32054cb2b6b62554bac62f18898d47dd98475
jktoiuhito <56585957+jktoiuhito@users.noreply.github.com>
2020-09-23 17:54:25 +0300

    separete html, js and css, make into a proper project

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+const req = new XMLHttpRequest();
+req.onreadystatechange = () => {
+  if (req.readyState === XMLHttpRequest.DONE) {
+    const quotes = JSON.parse(req.responseText);
+    const quote = quotes[Math.floor(Math.random() * Math.ceil(quotes.length))];
+    const blockquote = document.getElementById("quote");
+    const footer = document.createElement("footer");
+    const cite = document.createElement("cite");
+    blockquote.append(document.createTextNode(quote.quote), footer);
+    footer.append(
+      document.createTextNode(quote.author ? quote.author + ", " : ""),
+      cite
+    );
+    cite.append(document.createTextNode(quote.source));
+  }
+};
+req.open("GET", "quotes.json", true);
+req.send();
