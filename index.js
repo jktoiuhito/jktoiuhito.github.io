054787b7a677836e36492836cd37eb5a0307af2b
jktoiuhito <56585957+jktoiuhito@users.noreply.github.com>
2021-01-13 13:07:25 +0200

    refactor quote output, add next quote button

--- index.js
+++ index.js
@@ -2,18 +2,54 @@
 const req = new XMLHttpRequest();
 req.onreadystatechange = () => {
   if (req.readyState === XMLHttpRequest.DONE) {
+    // Parse quotes
     const quotes = JSON.parse(req.responseText);
-    const quote = quotes[Math.floor(Math.random() * Math.ceil(quotes.length))];
-    const blockquote = document.getElementById("quote");
-    const footer = document.createElement("footer");
-    footer.className = "blockquote-footer";
-    const cite = document.createElement("cite");
-    blockquote.append(document.createTextNode(quote.quote), footer);
-    footer.append(
-      document.createTextNode(quote.author ? quote.author + ", " : ""),
-      cite
+
+    // Get elements
+    const quoteTextParagraph = document.getElementById("quote-text-paragraph");
+    const quoteFooterFigcaption = document.getElementById(
+      "quote-footer-figcaption"
     );
-    cite.append(document.createTextNode(quote.source));
+    const quoteNextButton = document.getElementById("next-quote");
+
+    // Toggle element visibility
+    document.getElementById("quote-spinner").remove();
+    document.getElementById("quote-figure").hidden = false;
+    quoteNextButton.hidden = false;
+
+    const printQuote = () => {
+      // Choose a random quote
+      const quote =
+        quotes[Math.floor(Math.random() * Math.ceil(quotes.length))];
+
+      // Remove old quote
+      while (quoteTextParagraph.hasChildNodes()) {
+        quoteTextParagraph.removeChild(quoteTextParagraph.firstChild);
+      }
+      while (quoteFooterFigcaption.hasChildNodes()) {
+        quoteFooterFigcaption.removeChild(quoteFooterFigcaption.firstChild);
+      }
+
+      // Append quote text
+      quoteTextParagraph.append(document.createTextNode(quote.quote));
+
+      // Append quote author
+      const cite = document.createElement("cite");
+      cite.append(document.createTextNode(quote.source));
+      quoteFooterFigcaption.append(
+        document.createTextNode(quote.author ? quote.author + ", " : ""),
+        cite
+      );
+    };
+
+    // Print a quote
+    printQuote();
+
+    // Assign button to print a new quote
+    quoteNextButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      printQuote();
+    });
   }
 };
 req.open("GET", "quotes.json", true);
